7432b30d9d6587b370526c21ea177492add01f05
Peter H <peter.h@ibm.com>
2019-08-08 17:29:42 +0100

    Started drawing board

--- Python Files/pygameSetup.py
+++ Python Files/pygameSetup.py
@@ -1,24 +1,42 @@
+# Tutorial found on this website: https://ukdevguy.com/tutorial-on-how-to-draw-shapes-in-pygame/
+
 import pygame
 import sys
 # Create width and height constants
 WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_HEIGHT = 800
 # Initialise all the pygame modules
 pygame.init()
 # Create a game window
 game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # Set title
-pygame.display.set_caption("UKDevGuy tutorial")
+pygame.display.set_caption("Othello")
 game_running = True
 # Game loop
+
+##draw board
+
+for i in range(8):
+    for j in range(8):
+        rect = pygame.Rect(i*100,j*100,100,100)
+
+        if ((i % 2 == 0 & j % 2 == 0) | (i % 2 == 1 & j % 2 == 1)):
+            pygame.draw.rect(game_window,(255,255,255), rect)
+        else:
+            pygame.draw.rect(game_window,(0,0,0), rect)
+
+
+
+
+
 while game_running:
     # Loop through all active events
     for event in pygame.event.get():
+        
         # Close the program if the user presses the 'X'
         if event.type == pygame.QUIT:
             game_running = False
-    # Content here
-    game_window.fill((255,255,255))
+                   
     # Update our display
     pygame.display.update()
     
