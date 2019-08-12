8a0cc7f560c0cddf6d150aa13dd6035a2882665b
Peter H <peter.h@ibm.com>
2019-08-12 13:29:30 +0100

    Drawing board and start position

--- Python Files/pygameSetup.py
+++ Python Files/pygameSetup.py
@@ -2,43 +2,74 @@
 
 import pygame
 import sys
-# Create width and height constants
+
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
-# Initialise all the pygame modules
-pygame.init()
-# Create a game window
-game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-# Set title
-pygame.display.set_caption("Othello")
-game_running = True
-# Game loop
-
-##draw board
-
-for i in range(8):
-    for j in range(8):
-        rect = pygame.Rect(i*100,j*100,100,100)
-
-        if ((i % 2 == 0 & j % 2 == 0) | (i % 2 == 1 & j % 2 == 1)):
-            pygame.draw.rect(game_window,(255,255,255), rect)
-        else:
-            pygame.draw.rect(game_window,(0,0,0), rect)
 
 
+def generateBoard():
+    boardArray = []
 
-
-
-while game_running:
-    # Loop through all active events
-    for event in pygame.event.get():
+    for _ in range(8):
+        column = []
+        for _ in range(8):
+            column.append('x')
         
-        # Close the program if the user presses the 'X'
-        if event.type == pygame.QUIT:
-            game_running = False
-                   
-    # Update our display
-    pygame.display.update()
+        boardArray.append(column)
     
-# Uninitialize all pygame modules and quit the program
-pygame.quit()
+    boardArray = initialiseBoard(boardArray)
+    return boardArray
+
+
+def initialiseBoard(board):
+    board[3][3] = 'b'
+    board[4][4] = 'b'
+    board[3][4] = 'w'
+    board[4][3] = 'w'
+    return board
+
+
+def drawBoard(board, game_window):
+    game_window.fill((0,157,0))
+    for i in range(8):
+        for j in range(8):
+            rect = pygame.Rect(i*100,j*100,100,100)
+            pygame.draw.rect(game_window, (0,0,0), rect, 5)
+
+            pieceVal = board[i][j]
+
+            if pieceVal == 'w':
+                pygame.draw.circle(game_window, (255,255,255), (i*100 + 50, j*100 + 50), 40, 0)
+            elif pieceVal == 'b':
+                pygame.draw.circle(game_window, (0,0,0), (i*100 + 50, j*100 + 50), 40, 0)
+            
+
+def main():
+
+    board = generateBoard()
+    pygame.init()
+
+    # Create a game window
+    game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Othello")
+    game_running = True
+
+    drawBoard(board, game_window)
+
+
+    while game_running:
+        # Loop through all active events
+        for event in pygame.event.get():
+            
+            # Close the program if the user presses the 'X'
+            if event.type == pygame.QUIT:
+                game_running = False
+                    
+        # Update our display
+        pygame.display.update()
+        
+    # Uninitialize all pygame modules and quit the program
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
