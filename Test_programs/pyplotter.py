21494fe9888f9cac0e297591698860ca6ee30eac
Peter <peter.hessey@hotmail.com>
2020-02-25 13:17:24 +0000

    C-Val tests complete

--- Test_programs/pyplotter.py
+++ Test_programs/pyplotter.py
@@ -1,22 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import csv
+
+data_filepath = '../Results/CValTests.csv'
+
+c_values = []
+wins = []
+
+with open(data_filepath) as csv_data_file:
+    csv_reader = csv.reader(csv_data_file, delimiter=',')
+    row_num = 0
+
+    for row in csv_reader:
+        if row_num != 0:
+            c_values.append(float(row[0]))
+            wins.append(float(row[1]))
+        
+        row_num += 1
 
 
-wins = [50, 45, 14, 17]
-losses = [0, 4, 36, 31]
-draws = [0 , 1, 0, 2]
-tags = ['Random', 'Roxanne', 'MC-10', 'CNN-6']
-width = 0.3
-
-plt.xticks(np.arange(len(wins)) + width, tags)
-plt.xlabel('Agent')
-plt.ylabel('No. of games')
-plt.title('MC-1 game results (50 games)')
-plt.bar(np.arange(len(wins)), wins, width=width, label='Wins')
-plt.bar(np.arange(len(losses)) + width, losses, width=width, label='Losses')
-plt.bar(np.arange(len(draws)) + width * 2, draws, width=width, label='Ties')
-
-plt.legend(loc='upper right')
+plt.xlabel('C-Value')
+plt.ylabel('Number of wins against standard value')
+plt.title('Monte-Carloe C-Value testing')
+plt.scatter(c_values, wins)
 
 plt.show()
